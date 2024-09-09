5e754e2c718f74fe3dc96cc38d977077c9a2eb6f
Rens Korswagen <renskorswagen@users.noreply.github.com>
2024-09-09 12:56:43 +0200

    Make Context constructer final protected

--- src/ValueObject/Context.php
+++ src/ValueObject/Context.php
@@ -19,7 +19,7 @@ use WeDevelop\Audit\Util\TranslationHelper;
 
 readonly class Context implements TranslatableInterface
 {
-    private function __construct(
+    final protected function __construct(
         public AuditSource $source,
         public ?TokenInterface $token = null,
         public ?IpAddress $ip = null,
